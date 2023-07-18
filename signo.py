dc3201c695c23edd2f24943b312cafcdbfabc14e
Meel2M <melzinha622@gmail.com>
2023-07-17 21:08:05 -0300

    Introduzindo o projeto no git

---
+++ signo.py
@@ -0,0 +1,100 @@
+nome = str(input("Digite seu primeiro nome:"))
+sobrenome = str(input("Digite seu sobrenome:"))
+idade = int(input("Digite sua idade em valor numerico:"))
+signo = str(input("Qual seu signo?"))
+sonho = str(input("Nos conte seu sonho:"))
+
+if signo == "Aquário":
+    if signo : 
+      print(nome)
+      print("Horóscopo dia 18/07/2023 (terça)")
+      print("É muito bom quando as coisas se fazem meio que por si mesmas, com mínimo esforço, porque é assim que a alma idealiza existir, sem atrito, de forma confortável, levando ideias à prática com alegria e leveza.")
+    else:
+       print("Verifique se os dados estão corretos")
+elif signo == "Sagitario":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("Ainda que não se possa descrever, há um plano em andamento. O mesmo acontece com a vida, ninguém sabe o que ela é, mas a ignorância não impede de a vida ser vivida, ou que sejamos consciência sem saber o que ela é.")
+    else:
+       print("Verifique se os dados estão corretos")
+elif signo == "Escorpião":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("Há coisas que sua alma enxerga com muita clareza, mas que passam despercebidas para a maioria das pessoas, e não há como as convencer de isso ser diferente. Utilize as informações ao seu favor e com discrição.")
+    else:
+       print("Verifique se os dados estão corretos")
+elif signo == "Libra":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("De um jeito ou de outro, você verá as coisas andarem e isso será motivo de celebração, mesmo que, pelas regras da lógica, tudo deveria ser diferente. Entenda, estamos num mundo completamente diferente. Melhor assim.")
+    else:
+       print("Verifique se os dados estão corretos")
+elif signo == "Capricórnio":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("Faça o que estiver ao seu alcance para se relacionar melhor com as pessoas que servem de referência para sua alma, aquelas com as quais interiormente você dialoga bastante. Faça gestos concretos de aproximação.")
+    else:
+       print("Verifique se os dados estão corretos")
+
+elif signo == "Gêmeos":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("O pouco de prático e de concreto que você fizer para resolver as pendências será o muito de avanço que você colherá no final do dia. Vale a pena se mexer, porque ainda que a ação seja atrapalhada, será valiosa.")
+    else:
+       print("Verifique se os dados estão corretos")
+
+elif signo == "Peixes":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("Vale a pena você restaurar os princípios mais nobres e elevados que foram imaginados algum dia, e que fazem seu coração arder de vontade de viver de acordo com esses. Este é um momento de elevação, aproveite tudo.")
+    else:
+       print("Verifique se os dados estão corretos")
+
+elif signo == "Cancêr":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("A banalidade é necessária também, porque a alma não suporta viver se questionando eternamente, ela precisa relaxar. Quando as ideias e as práticas se aproximam, naturalmente a banalidade é deixada de lado, por ser inútil.")
+    else:
+       print("Verifique se os dados estão corretos")
+
+elif signo == "Virgem":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("Aos poucos, você anda formando uma nova turma que lhe sirva de referência, para pensar junto, para se preocupar com as mesmas coisas, para servir, inclusive, de ponto de apoio para a elaboração de conflitos necessários.")
+    else:
+       print("Verifique se os dados estão corretos")
+
+elif signo == "Leão":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("Reserve um bom tempo para conversar sinceramente com sua própria alma, observando com o maior detalhamento possível a natureza dos desejos que imprimem urgência, e que por isso produzem emoções muito intensas. Conhecer.")
+    else:
+       print("Verifique se os dados estão corretos")
+       
+elif signo == "Touro":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("É um bom dia para refletir sobre sua vida amorosa e fazer perguntas em toda intimidade. Você pode aproveitar algumas ocasiões para examinar mais de perto a qualidade e a profundidade de seus afetos!")
+    else:
+       print("Verifique se os dados estão corretos")
+       
+elif signo == "Áries":
+    if signo :
+     print(nome)
+     print("Horóscopo dia 18/07/2023 (terça)")
+     print("Os bons sentimentos são telepáticos, são recebidos pelas pessoas a que são dirigidos, com a mesma intensidade com que os maus sentimentos também se transmitem assim.")
+    else:
+       print("Verifique se os dados estão corretos")
+
+
+       
